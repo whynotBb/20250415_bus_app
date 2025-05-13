321eefc4ac02f74f4368ad383506c704df4f1ce3
whynotBb <whynot.byoul@gmail.com>
2025-05-13 17:46:34 +0900

    포매터 수정사항 발생

--- src/pages/HomePage/components/Map/BusStopDetail.tsx
+++ src/pages/HomePage/components/Map/BusStopDetail.tsx
@@ -11,92 +11,92 @@ import dayjs from "dayjs";
 import { normalizeItemList } from "../../../../utils/data/normalizeItemList";
 
 interface BusStopDetailProps {
-  isOpen: boolean;
-  stationInfo: StationItem | null;
-  openBusDetail: (busDetailInfo: StationByUidItem) => void;
+	isOpen: boolean;
+	stationInfo: StationItem | null;
+	openBusDetail: (busDetailInfo: StationByUidItem) => void;
 }
 const BottomSheet = styled("div")(({ theme }) => ({
-  width: "100%",
-  maxWidth: "768px",
-  height: "300px",
-  position: "fixed",
-  bottom: "-300px",
-  left: "50%",
-  transform: "translateX(-50%)",
-  backgroundColor: theme.palette.background.default,
-  borderRadius: "30px 30px 0 0",
-  boxShadow: "0px -4px 10px 2px rgba(0, 0, 0, .3)",
-  padding: "20px",
-  transition: "all .3s",
-  overflowY: "auto",
+	width: "100%",
+	maxWidth: "768px",
+	height: "300px",
+	position: "fixed",
+	bottom: "-300px",
+	left: "50%",
+	transform: "translateX(-50%)",
+	backgroundColor: theme.palette.background.default,
+	borderRadius: "30px 30px 0 0",
+	boxShadow: "0px -4px 10px 2px rgba(0, 0, 0, .3)",
+	padding: "20px",
+	transition: "all .3s",
+	overflowY: "auto",
 }));
 
 const Title = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexWrap: "wrap",
-  gap: "5px",
-  alignItems: "center",
-  b: {
-    color: theme.palette.primary.main,
-    fontSize: "18px",
-  },
+	display: "flex",
+	flexWrap: "wrap",
+	gap: "5px",
+	alignItems: "center",
+	b: {
+		color: theme.palette.primary.main,
+		fontSize: "18px",
+	},
 }));
 const DetailList = styled("div")(({ theme }) => ({
-  margin: "14px 0 10px",
-  ".item ": {
-    width: "calc(50% - 3px)",
-    textAlign: "center",
-  },
-  small: {
-    display: "block",
-    fontWeight: "400",
-    marginBottom: "4px",
-  },
-  ".swiper-button-prev:after,.swiper-button-next:after": {
-    fontSize: "20px",
-    color: theme.palette.primary.main,
-  },
+	margin: "14px 0 10px",
+	".item ": {
+		width: "calc(50% - 3px)",
+		textAlign: "center",
+	},
+	small: {
+		display: "block",
+		fontWeight: "400",
+		marginBottom: "4px",
+	},
+	".swiper-button-prev:after,.swiper-button-next:after": {
+		fontSize: "20px",
+		color: theme.palette.primary.main,
+	},
 }));
 const BusName = styled("b")({
-  display: "inline-block",
-  padding: "0 10px",
-  borderRadius: "100px",
-  fontSize: "16px",
-  marginBottom: "6px",
-  height: "26px",
-  lineHeight: "27px",
-  background: "gray",
-  color: "#fff",
-  "&.skyBlue": {
-    backgroundColor: "#007ACC", // Deep Sky Blue
-  },
-  "&.green": {
-    backgroundColor: "#219653", // Deep Emerald Green
-  },
-  "&.blue": {
-    backgroundColor: "#2F80ED", // Vivid Royal Blue
-  },
-  "&.yellow": {
-    backgroundColor: "#E2B93B", // Rich Golden Yellow (어두운 편에 속하는 노란색)
-  },
-  "&.red": {
-    backgroundColor: "#C0392B", // Dark Red
-  },
+	display: "inline-block",
+	padding: "0 10px",
+	borderRadius: "100px",
+	fontSize: "16px",
+	marginBottom: "6px",
+	height: "26px",
+	lineHeight: "27px",
+	background: "gray",
+	color: "#fff",
+	"&.skyBlue": {
+		backgroundColor: "#007ACC", // Deep Sky Blue
+	},
+	"&.green": {
+		backgroundColor: "#219653", // Deep Emerald Green
+	},
+	"&.blue": {
+		backgroundColor: "#2F80ED", // Vivid Royal Blue
+	},
+	"&.yellow": {
+		backgroundColor: "#E2B93B", // Rich Golden Yellow (어두운 편에 속하는 노란색)
+	},
+	"&.red": {
+		backgroundColor: "#C0392B", // Dark Red
+	},
 });
 const UpdateTime = styled("div")({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  gap: "6px",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "flex-end",
+	gap: "6px",
 });
 
 const Timer = styled("div")({
-  width: "30px",
-  height: "30px",
-  lineHeight: "31px",
-  textAlign: "center",
-  borderRadius: "50%",
-  position: "relative",
+	width: "30px",
+	height: "30px",
+	lineHeight: "31px",
+	textAlign: "center",
+	borderRadius: "50%",
+	position: "relative",
 });
 // 회전 애니메이션 정의
 const spin = keyframes`
@@ -105,140 +105,122 @@ const spin = keyframes`
 `;
 
 const RotatingCircle = styled("span")(({ theme }) => ({
-  content: '""',
-  position: "absolute",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  border: `2px dashed ${theme.palette.primary.main}`,
-  borderRadius: "50%",
-  borderTopColor: "transparent",
-  borderRightColor: "transparent",
-  animation: `${spin} 2s linear infinite`,
-  pointerEvents: "none",
+	content: '""',
+	position: "absolute",
+	top: 0,
+	left: 0,
+	width: "100%",
+	height: "100%",
+	border: `2px dashed ${theme.palette.primary.main}`,
+	borderRadius: "50%",
+	borderTopColor: "transparent",
+	borderRightColor: "transparent",
+	animation: `${spin} 2s linear infinite`,
+	pointerEvents: "none",
 }));
 
 const BusStopDetail = ({ isOpen, stationInfo, openBusDetail }: BusStopDetailProps) => {
-  const arsId = stationInfo?.arsId ?? 0;
-  // refetch > react-query 에서 제공하는 기능
-  const { data, refetch, isLoading } = useGetStationByUidItem(arsId);
-  const busInfo = data?.ServiceResult?.msgBody;
-  //update time
+	const arsId = stationInfo?.arsId ?? 0;
+	// refetch > react-query 에서 제공하는 기능
+	const { data, refetch, isLoading } = useGetStationByUidItem(arsId);
+	const busInfo = data?.ServiceResult?.msgBody;
+	//update time
 
-  const [lastUpdated, setLastUpdated] = useState<string>("");
-  const [countDown, setCountDown] = useState<number>(15);
+	const [lastUpdated, setLastUpdated] = useState<string>("");
+	const [countDown, setCountDown] = useState<number>(15);
 
-  //3초에 한번씩 버스 정보 업데이트
-  useEffect(() => {
-    if (!arsId) return;
+	//15초에 한번씩 버스 정보 업데이트
+	useEffect(() => {
+		if (!arsId) return;
 
-    // 현재 시간 표기, 데이터 refetch 할때 마다 업데이트
-    const update = () => {
-      refetch();
-      const now = dayjs().format("YY.MM.DD ddd HH:mm:ss");
-      setLastUpdated(now);
-      setCountDown(15);
-    };
+		// 현재 시간 표기, 데이터 refetch 할때 마다 업데이트
+		const update = () => {
+			refetch();
+			const now = dayjs().format("YY.MM.DD ddd HH:mm:ss");
+			setLastUpdated(now);
+			setCountDown(15);
+		};
 
-    update(); // 컴포넌트 처음 마운트될 때도 호출
-    const interval = setInterval(update, 15000);
+		update(); // 컴포넌트 처음 마운트될 때도 호출
+		const interval = setInterval(update, 15000);
 
-    return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 정리
-  }, [arsId, refetch]);
+		return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 정리
+	}, [arsId, refetch]);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCountDown((prev) => (prev > 1 ? prev - 1 : 15));
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCountDown((prev) => (prev > 1 ? prev - 1 : 15));
+		}, 1000);
+		return () => clearInterval(timer);
+	}, []);
 
-  useEffect(() => {
-    if (!stationInfo) return;
+	useEffect(() => {
+		if (!stationInfo) return;
 
-    console.log("bus stop detail : ", busInfo);
+		console.log("bus stop detail : ", busInfo);
 
-    if (!busInfo) {
-      console.log("! busInfo");
-    } else if (busInfo === undefined) {
-      console.log("bus info undefined");
-    }
-  }, [stationInfo, busInfo]);
-  const itemList = normalizeItemList(busInfo?.itemList).sort((a, b) => {
-    const getSortValue = (item: StationByUidItem) => {
-      if (!item.traTime1 || item.arrmsg1 === "운행종료") {
-        return Infinity;
-      }
-      return item.traTime1;
-    };
+		if (!busInfo) {
+			console.log("! busInfo");
+		} else if (busInfo === undefined) {
+			console.log("bus info undefined");
+		}
+	}, [stationInfo, busInfo]);
+	const itemList = normalizeItemList(busInfo?.itemList).sort((a, b) => {
+		const getSortValue = (item: StationByUidItem) => {
+			if (!item.traTime1 || item.arrmsg1 === "운행종료") {
+				return Infinity;
+			}
+			return item.traTime1;
+		};
 
-    return getSortValue(a) - getSortValue(b);
-  });
-  if (isLoading) return <Loading />;
-  return (
-    <>
-      <BottomSheet style={{ bottom: isOpen ? "0" : "-300px" }}>
-        <Title>
-          <Typography variant="h2">
-            <b>{stationInfo?.stationNm}</b> <small>{stationInfo?.dist}m</small>
-          </Typography>
-          <Typography>
-            ({stationInfo?.arsId} / ID : {stationInfo?.stationId})
-          </Typography>
-        </Title>
-        <DetailList>
-          <Swiper navigation={true} modules={[Navigation]} slidesPerView={2} spaceBetween={6} className="mySwiper">
-            {itemList.length > 0 ? (
-              itemList.map((item, idx) => (
-                <SwiperSlide className="item" key={idx} onClick={() => openBusDetail({ ...item })}>
-                  <div>
-                    <BusName
-                      className={
-                        item.routeType === "1"
-                          ? "skyBlue"
-                          : item.routeType === "2"
-                          ? "green"
-                          : item.routeType === "3"
-                          ? "blue"
-                          : item.routeType === "4"
-                          ? "green"
-                          : item.routeType === "5"
-                          ? "yellow"
-                          : item.routeType === "6"
-                          ? "red"
-                          : "gray"
-                      }
-                    >
-                      {item.rtNm}
-                    </BusName>
-                    <small>다음 : {item.nxtStn}</small>
-                    <small>({item.adirection} 방면)</small>
-                    <p>
-                      {item.arrmsg1} {item.isFullFlag1 === 1 && "(만차)"}
-                    </p>
-                    <p>
-                      {item.arrmsg2} {item.isFullFlag1 === 1 && "(만차)"}
-                    </p>
-                  </div>
-                </SwiperSlide>
-              ))
-            ) : (
-              <Loading />
-            )}
-          </Swiper>
-        </DetailList>
-        <UpdateTime>
-          <p>{lastUpdated}</p>
+		return getSortValue(a) - getSortValue(b);
+	});
+	if (isLoading) return <Loading />;
+	return (
+		<>
+			<BottomSheet style={{ bottom: isOpen ? "0" : "-300px" }}>
+				<Title>
+					<Typography variant="h2">
+						<b>{stationInfo?.stationNm}</b> <small>{stationInfo?.dist}m</small>
+					</Typography>
+					<Typography>
+						({stationInfo?.arsId} / ID : {stationInfo?.stationId})
+					</Typography>
+				</Title>
+				<DetailList>
+					<Swiper navigation={true} modules={[Navigation]} slidesPerView={2} spaceBetween={6} className="mySwiper">
+						{itemList.length > 0 ? (
+							itemList.map((item, idx) => (
+								<SwiperSlide className="item" key={idx} onClick={() => openBusDetail({ ...item })}>
+									<div>
+										<BusName className={item.routeType === "1" ? "skyBlue" : item.routeType === "2" ? "green" : item.routeType === "3" ? "blue" : item.routeType === "4" ? "green" : item.routeType === "5" ? "yellow" : item.routeType === "6" ? "red" : "gray"}>{item.rtNm}</BusName>
+										<small>다음 : {item.nxtStn}</small>
+										<small>({item.adirection} 방면)</small>
+										<p>
+											{item.arrmsg1} {item.isFullFlag1 === 1 && "(만차)"}
+										</p>
+										<p>
+											{item.arrmsg2} {item.isFullFlag1 === 1 && "(만차)"}
+										</p>
+									</div>
+								</SwiperSlide>
+							))
+						) : (
+							<Loading />
+						)}
+					</Swiper>
+				</DetailList>
+				<UpdateTime>
+					<p>{lastUpdated}</p>
 
-          <Timer>
-            <RotatingCircle />
-            {countDown}
-          </Timer>
-        </UpdateTime>
-      </BottomSheet>
-    </>
-  );
+					<Timer>
+						<RotatingCircle />
+						{countDown}
+					</Timer>
+				</UpdateTime>
+			</BottomSheet>
+		</>
+	);
 };
 
 export default BusStopDetail;
