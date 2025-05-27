e4e80c9f3f8297bcf5e10a81e000bf192506c689
whynotBb <whynot.byoul@gmail.com>
2025-05-27 09:37:36 +0900

    파라미터 준비 되면 호출하도록 예외처리

--- src/hooks/useGetUltraSrtFcst.ts
+++ src/hooks/useGetUltraSrtFcst.ts
@@ -3,9 +3,11 @@ import { UltraSrtNcstReq } from "../models/weather";
 import { getUltraSrtFcst } from "../apis/weatherApi";
 
 const useGetUltraSrtFcst = (params: UltraSrtNcstReq) => {
+	const isReady = !!params.base_date && !!params.base_time && params.nx !== undefined && params.ny !== undefined;
 	return useQuery({
 		queryKey: ["ultra-srt-fcst", params],
 		queryFn: () => getUltraSrtFcst(params),
+		enabled: isReady, // ✅ 파라미터 준비 여부 확인
 	});
 };
 export default useGetUltraSrtFcst;
